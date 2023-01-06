20a55a39bad64ebc274c48e336fa6d87da099630
egyptiank <91704088+egyptiank@users.noreply.github.com>
2023-01-07 00:20:21 +0300

    atwar

---
+++ wp-content/plugins/ohio-extra/elementor/widgets/accordion/handler.js
@@ -0,0 +1,1 @@
+jQuery(window).on('elementor/frontend/init',()=>{elementorFrontend.hooks.addAction('frontend/element_ready/ohio_accordion.default',($element)=>{jQuery(window).trigger('ohio:handle_accordion_box');jQuery(window).trigger('ohio:handle_accordion_box_size');});});
---
+++ wp-content/plugins/ohio-extra/elementor/widgets/counter/handler.js
@@ -0,0 +1,1 @@
+jQuery(window).on('elementor/frontend/init',()=>{elementorFrontend.hooks.addAction('frontend/element_ready/ohio_counter.default',($element)=>{jQuery(window).trigger('ohio:handle_counter_box');});});
